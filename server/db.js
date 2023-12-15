3a8af0d3dd99c7a02729f4a1ce8a833917784132
LexusLFA220 <alex.amin@lc.cuny.edu>
2023-12-15 10:10:19 -0500

    created paths but not completley working

--- server/db.js
+++ server/db.js
@@ -1,8 +1,44 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: '<username>',
+  user: 'alexa',
   host: 'localhost',
   database: 'favlinks',
-  password: '<password>',
+  password: 'lexus',
   port: 5432,
 })
+
+ //Get all links from db
+ const getLinks = (req, res) => {
+  pool.query('SELECT * FROM favlinks ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+const createLink = (req, res) => {
+  const name = request.body.name
+  const url = request.body.url
+  pool.query('INSERT INTO favlinks (name,url) VALUES ($1,$2)',[name,url], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Link added with ID: ${results.rows[0].id}`)
+  })
+}
+
+const deleteLink = (req, res) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM favlinks WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).send(`Link deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getLinks,createLink,deleteLink
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
+//const cors = require('cors')
 const path = require('path')
+
+const db = require('./db')
 
 const app = express();
 
@@ -9,12 +11,17 @@ const PORT = 3000
 const clientPath = path.resolve(__dirname, '../client/dist')
 
 app.use(express.static(clientPath))
-
+app.use(express.json())
+//app.use(cors())
+//Routes
 app.get('/', (req, res) => {
     // we'll do some stuff here
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
   })
 
+  app.get('/api/links', db.getLinks)
+  app.post('/api/links', db.createLink)
+  app.delete('/api/links/id:', db.deleteLink)
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
